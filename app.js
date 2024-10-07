ee7d752558e804e844cbffa72b2bebe7b0055252
Emanuel Isaac Afanador Pacheco <eiafanad@bancolombia.com.co>
2024-10-07 10:57:57 -0500

    add fix

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-function upload(fileinput) {
-  var image = new SimpleImage(fileinput);
-  var canvas = document.getElementById("can");
-  image.drawTo(canvas);
+function upload(inputFile) {
+  console.log(inputFile.files)
+  var FR = new FileReader();
+  var canvas_ = document.getElementById("can");
+  var ctx_ = canvas_.getContext("2d");
+  FR.onload = function (e) {
+    var img = new Image();
+    var result = e.target.result;
+    img.src = result;
+    img.onload = function () {
+      canvas_.width = this.width;
+      canvas_.height = this.height;
+      ctx_.drawImage(this, 0, 0,
+        this.width, this.height);
+    };
+  };
+  FR.readAsDataURL(inputFile.files[0]);
 }
